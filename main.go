eb43deada3f70204596270c2a3104ea7f45f79ab
r3g492 <kangggunw3492@gmail.com>
2025-04-08 02:18:01 +0900

    add camera

--- main.go
+++ main.go
@@ -8,12 +8,33 @@ func main() {
 
 	rl.SetTargetFPS(60)
 
+	camera := rl.Camera3D{
+		Position: rl.Vector3{
+			X: 10,
+			Y: 10,
+			Z: 0,
+		},
+		Target: rl.Vector3{
+			X: 0,
+			Y: 0,
+			Z: 0,
+		},
+		Up: rl.Vector3{
+			X: 1,
+			Y: 10,
+			Z: 0,
+		},
+		Fovy:       90,
+		Projection: 0,
+	}
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
+		rl.DrawFPS(100, 100)
+		rl.BeginMode3D(camera)
+		rl.DrawGrid(10, 10)
+		rl.EndMode3D()
 		rl.EndDrawing()
 	}
 }
