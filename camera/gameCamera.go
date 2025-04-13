faf1788146d3cf243b40450fe29c3946c62b97c9
r3g492 <kangggunw3492@gmail.com>
2025-04-13 23:59:17 +0900

    add camera module

---
+++ camera/gameCamera.go
@@ -0,0 +1,14 @@
+package gameCamera
+
+var (
+	PositionX float32 = 100
+	PositionY float32 = 100
+	PositionZ float32 = 0
+	TargetX   float32 = 0
+	TargetY   float32 = 0
+	TargetZ   float32 = 0
+	UpX       float32 = 0
+	UpY       float32 = 1
+	UpZ       float32 = 0
+	Fovy      float32 = 90
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	gameCamera "MysteryGameJam2025/camera"
 	"MysteryGameJam2025/game"
 	gameMath "MysteryGameJam2025/math"
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -14,64 +15,21 @@ var (
 )
 
 func main() {
-	rl.InitWindow(
-		int32(screenWidth),
-		int32(screenHeight),
-		"MysteryGameJam2025",
-	)
+	rl.InitWindow(int32(screenWidth), int32(screenHeight), "MysteryGameJam2025")
+	rl.ToggleFullscreen()
 	defer rl.CloseWindow()
-
 	rl.SetTargetFPS(60)
 
-	camera := rl.Camera3D{
-		Position: rl.Vector3{
-			X: 10,
-			Y: 10,
-			Z: 0,
-		},
-		Target: rl.Vector3{
-			X: 0,
-			Y: 0,
-			Z: 0,
-		},
-		Up: rl.Vector3{
-			X: 1,
-			Y: 10,
-			Z: 0,
-		},
-		Fovy:       90,
-		Projection: 0,
-	}
-
-	game.CreatePlayer(
-		game.Player{
-			X: 0, Y: 0, Z: 0,
-			Alive: true,
-		},
-	)
-	var player = game.GetPlayer()
+	// Create the player and retrieve its pointer/instance.
+	game.CreatePlayer(game.Player{
+		X:     0,
+		Y:     0,
+		Z:     0,
+		Alive: true,
+	})
+	player := game.GetPlayer()
 
 	for !rl.WindowShouldClose() {
-		camera = rl.Camera3D{
-			Position: rl.Vector3{
-				X: 10,
-				Y: 10,
-				Z: 0,
-			},
-			Target: rl.Vector3{
-				X: player.X,
-				Y: player.Y,
-				Z: player.Z,
-			},
-			Up: rl.Vector3{
-				X: 1,
-				Y: 10,
-				Z: 0,
-			},
-			Fovy:       90,
-			Projection: 0,
-		}
-
 		if player.Alive {
 			mouseDelta := rl.GetMouseDelta()
 			screenYaw += mouseDelta.X * 0.005
@@ -83,47 +41,58 @@ func main() {
 				screenPitch = -1.5
 			}
 
-			radius := float32(10.0)
-			camX := radius * gameMath.Cos(screenPitch) * gameMath.Sin(screenYaw)
-			camY := radius * gameMath.Sin(screenPitch)
-			camZ := radius * gameMath.Cos(screenPitch) * gameMath.Cos(screenYaw)
-
-			camera.Position = rl.Vector3{
-				X: player.X + camX,
-				Y: player.Y + camY,
-				Z: player.Z + camZ,
-			}
-			camera.Target = rl.Vector3{
-				X: player.X,
-				Y: player.Y,
-				Z: player.Z,
-			}
-			camera.Up = rl.NewVector3(0, 1, 0)
-
-			var cursorRay = rl.GetScreenToWorldRay(
-				rl.Vector2{
-					X: screenWidth / 2,
-					Y: screenHeight / 2,
-				},
-				camera,
-			)
-
-			camera.Target = rl.Vector3{
-				X: player.X + cursorRay.Direction.X*100,
-				Y: player.Y + cursorRay.Direction.Y*100,
-				Z: player.Z + cursorRay.Direction.Z*100,
+			if rl.IsKeyDown(rl.KeyW) {
+				// Movement logic can go here.
 			}
 
-			// rl.DrawLine3D(player.Position(), target, rl.Red)
+			gameCamera.PositionX = player.X
+			gameCamera.PositionY = player.Y
+			gameCamera.PositionZ = player.Z
+
+			dirX := gameMath.Cos(screenPitch) * gameMath.Sin(screenYaw)
+			dirY := gameMath.Sin(screenPitch)
+			dirZ := gameMath.Cos(screenPitch) * gameMath.Cos(screenYaw)
+
+			gameCamera.TargetX = player.X + dirX
+			gameCamera.TargetY = player.Y + dirY
+			gameCamera.TargetZ = player.Z + dirZ
 		}
+
+		camera := updateCamera()
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 		rl.DrawFPS(100, 100)
+
 		rl.BeginMode3D(camera)
-		rl.DrawGrid(10, 10)
+		rl.DrawGrid(100, 100)
+
+		rl.DrawCube(rl.Vector3{X: 0, Y: 0.5, Z: 3}, 1, 1, 1, rl.Blue)
 		rl.EndMode3D()
+
 		rl.EndDrawing()
+	}
+}
+
+func updateCamera() rl.Camera3D {
+	return rl.Camera3D{
+		Position: rl.Vector3{
+			X: gameCamera.PositionX,
+			Y: gameCamera.PositionY,
+			Z: gameCamera.PositionZ,
+		},
+		Target: rl.Vector3{
+			X: gameCamera.TargetX,
+			Y: gameCamera.TargetY,
+			Z: gameCamera.TargetZ,
+		},
+		Up: rl.Vector3{
+			X: gameCamera.UpX,
+			Y: gameCamera.UpY,
+			Z: gameCamera.UpZ,
+		},
+		Fovy:       gameCamera.Fovy,
+		Projection: rl.CameraPerspective,
 	}
 }
